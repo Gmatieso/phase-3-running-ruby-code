7dae6f178489167b89dd99c13826ec3644cfcf02
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-15 16:03:37 +0300

    :construction:  First Hello World

---
+++ spec/app.rb
@@ -0,0 +1,6 @@
+puts "Hello Word!"
+
+#in ruby using parentheses when invorking a method is optional this can also be done like so.
+puts("Hello World")
+#Both these syntax are plesant but in rubby the first is usually determined since its good to look at  
+#unlike JavaScript will be using ruby interpreter ro run our code in the terminal 
